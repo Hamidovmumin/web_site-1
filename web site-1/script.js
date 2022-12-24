a4e2dcd858f223ed59c54d185efb31a98e099ef1
Hamidovmumin <93463742+Hamidovmumin@users.noreply.github.com>
2022-12-24 12:57:42 +0400

    Add files via upload

---
+++ web site-1/script.js
@@ -0,0 +1,62 @@
+$('.toggle').on('click', function() {
+    $('.container').stop().addClass('active');
+  });
+  
+  
+  $('.close').on('click', function() {
+    $('.container').stop().removeClass('active');
+  });
+
+
+  const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+
+const enable = (e) => {
+  document.getElementById('burger').classList.toggle("open");
+  document.getElementById("nav").classList.toggle("nav-open");
+};
+
+$('.toggle').on('click', function() {
+  $('.container').stop().addClass('active');
+});
+
+$('.close').on('click', function() {
+  $('.container').stop().removeClass('active');
+});
